80ef876feda595f61e6c021eb7fd906513c17e3d
filiphertzman <filip.hertzman@gmail.com>
2023-03-09 13:28:01 +0100

    Initial Commit

---
+++ NoteCoreData/NoteCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteCoreDataApp.swift
+//  NoteCoreData
+//
+//  Created by Filip Hertzman on 2023-03-09.
+//
+
+import SwiftUI
+
+@main
+struct NoteCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
